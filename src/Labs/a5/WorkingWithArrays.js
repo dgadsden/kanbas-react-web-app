9b708490ad110b0c8810b0b3339e357b1a92f59a
dgadsden <99849542+dgadsden@users.noreply.github.com>
2023-11-21 22:00:43 -0500

    update api links

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -70,7 +70,7 @@ function WorkingWithArrays() {
     };
 
 
-    const API = "http://localhost:4000/a5/todos";
+    const API = "https://kanbas-node-server-app-nxoa.onrender.com/a5/todos";
     return (
         <div>
             <h3>Working with Arrays</h3>
