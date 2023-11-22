716022f40178f3f9ea47de611d0f8177abdbd4aa
dgadsden <99849542+dgadsden@users.noreply.github.com>
2023-11-21 22:53:38 -0500

    a5 deliverables

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -28,11 +28,11 @@ function Dashboard({ courses }
         {courses.map((course) => (
           <Link key={course._id} to={`/Kanbas/courses/${course._id}`} className="link-class">
             <div className="card" style={{ width: '18rem' }}>
-              {/* <img className="card-img-top" src={course.courseImage ? process.env.PUBLIC_URL + course.courseImage : process.env.PUBLIC_URL + 'images/stock_image.png'} alt={`Class Image for ${course.name}`} /> */}
+              <img className="card-img-top" src={course.courseImage ? process.env.PUBLIC_URL + course.courseImage : process.env.PUBLIC_URL + 'images/stock_image.png'} alt={`Class Image for ${course.name}`} />
               <div className="card-body">
                 <h5 className="card-title">{course.number}</h5>
                 <p className="card-text">{course.name}</p>
-                {/* <a href={`/Kanbas/Courses/${course._id}`} > <FaFileAlt style={{ fontSize: 25, color: "gray" }} /></a> */}
+                <a href={`/Kanbas/Courses/${course._id}`} > <FaFileAlt style={{ fontSize: 25, color: "gray" }} /></a>
               </div>
             </div>
           </Link>
